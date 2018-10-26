5a83bac2df9d1d6e782bb299ab1ea3de8d07e1f1
Sunanda <sgamage2@uwo.ca>
2018-10-25 20:05:36 -0400

    Add Bear model JS file, add GET, POST routes for /api/bears

--- bears_webservice/server.js
+++ bears_webservice/server.js
@@ -7,6 +7,8 @@ var app        = express();   // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
 
+var Bear     = require('./app/models/bear'); // contains Bear schema
+
 // configure app to use bodyParser (this will let us get the data from a POST)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,12 +23,50 @@ var mondodb_url = 'mongodb://localhost:27017/bears';
 
 var router = express.Router();   // get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+   // do things like logging, keeping statistics, validating input etc
+   console.log('In router.use()');
+   next(); // Important: make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:port/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'Hooray! welcome to our api!' });   
+   console.log('In router.get(): received GET request to /');
+   res.json({ message: 'Hooray! welcome to our api!' });   
 });
 
 // more routes for our API will happen here
+
+// ----------------------------------------------------
+router.route('/bears')
+    // create a bear (accessed at POST http://localhost:port/api/bears)
+   .post(function(req, res) {
+      var bear_name = req.body.name;     // get the bears name (comes from the request)
+      console.log('In router.route.post(): received POST request to /bears. name=' + bear_name);
+
+      var bear = new Bear();      // create a new instance of the Bear model
+      bear.name = bear_name;
+
+      // save the bear and check for errors
+      bear.save(function(err) {
+         if (err) {
+            res.send(err);
+         }
+         console.log('In router.route.post(): saved bear instance to DB');
+         res.json({ message: 'Bear created!' });
+      });
+   })
+    // get all the bears (accessed at GET http://localhost:port/api/bears)
+   .get(function(req, res) {
+      Bear.find(function(err, bears) {
+         if (err) {
+            res.send(err);
+         }
+         res.json(bears);
+      });
+  });
+
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
